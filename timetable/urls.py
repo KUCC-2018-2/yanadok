f1d31cb9943df0d9b9a977fefc61113688f2f9c8
kucc <anamkucc314@gmail.com>
2019-02-28 22:31:43 +0900

    Remove course list api url mapping from timetable app

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from course.views.api import CourseListApiView
 from timetable.views.api import TimetableApiView
 from timetable.views.timetable import TimetableView, UpdateTimetableView
 
@@ -8,6 +7,5 @@ app_name = 'timetable'
 urlpatterns = [
     path('', TimetableView.as_view(), name='home'),
     path('update', UpdateTimetableView.as_view(), name='update_timetble'),
-    path('test', TimetableApiView.as_view(), name='update_timetable'),
-    path('course', CourseListApiView.as_view(), name='course'),
+    path('timetable', TimetableApiView.as_view(), name='update_timetable')
 ]
