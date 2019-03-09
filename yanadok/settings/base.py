81390843218e67ce56de2c439708c6eefe6614d1
kucc <anamkucc314@gmail.com>
2019-03-09 18:50:43 +0900

    Change static root path

--- yanadok/settings/base.py
+++ yanadok/settings/base.py
@@ -107,7 +107,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 AUTH_USER_MODEL = 'user.User'
 
